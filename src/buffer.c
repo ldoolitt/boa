e29f4e1d884907f610fda9aa8a7d2cd1bef1e95a
Jon Nelson <jnelson@boa.org>
2007-08-12 23:45:21 -0500

    - add OFF_T_FORMAT in buffer.c log message
    Conflicts:
    	src/buffer.c

--- src/buffer.c
+++ src/buffer.c
@@ -43,7 +43,7 @@ int req_write(request * req, const char *msg)
     if (req->buffer_end + msg_len > sizeof(req->buffer)) {
         log_error_doc(req);
         fprintf(stderr, "There is not enough room in the buffer to"
-                " copy %u bytes (%ld available). Shutting down connection.\n",
+                " copy %u bytes (" OFF_T_FORMAT " available). Shutting down connection.\n",
                 msg_len,
                 sizeof(req->buffer) - req->buffer_end);
 #ifdef FASCIST_LOGGING
